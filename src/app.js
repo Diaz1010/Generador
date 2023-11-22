857b595a397085219ce14c70a29d18d4bc40e194
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2023-11-22 19:49:06 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = () => {
+  let who = ["Mi perro", "Mi Mama", "El vecino", "Mi amor"];
+  let what = [
+    "se enfermo",
+    "se estrello",
+    "lo robaron",
+    "Lo raptaron",
+    "Se me escapo",
+    "Estropeo todo"
+  ];
+  let when = [
+    "antes de salir de casa",
+    "en la calle",
+    "en el centro comercial",
+    "En la fiesta del Viernes"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return who[whoIndx] + " " + what[whatIndx] + " " + when[whenIndx];
+};
+window.onload = function() {
+  //write your code here
+  document.querySelector("#la-excusa").innerHTML = generateExcuse();
 };
